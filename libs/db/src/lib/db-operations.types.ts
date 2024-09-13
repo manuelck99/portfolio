c93e82a3658a0c5a383c1e711fdc8f4853462589
Manuel Can Kaya <manuelck99@hotmail.com>
2024-09-13 20:23:16 +0200

    Updated db operation types

--- libs/db/src/lib/db-operations.types.ts
+++ libs/db/src/lib/db-operations.types.ts
@@ -1,6 +1,33 @@
 import { Insertable, Selectable, Updateable } from "kysely"
-import { User } from "./db-schema.types"
+import {
+  Project,
+  ProjectFeature,
+  ProjectImage,
+  ProjectResponsibility,
+  Technology,
+  User,
+} from "./db-schema.types"
 
 export type SelectUser = Selectable<User>
 export type InsertUser = Insertable<User>
 export type UpdateUser = Updateable<User>
+
+export type SelectProject = Selectable<Project>
+export type InsertProject = Insertable<Project>
+export type UpdateProject = Updateable<Project>
+
+export type SelectProjectResponsibility = Selectable<ProjectResponsibility>
+export type InsertProjectResponsibility = Insertable<ProjectResponsibility>
+export type UpdateProjectResponsibility = Updateable<ProjectResponsibility>
+
+export type SelectProjectImage = Selectable<ProjectImage>
+export type InsertProjectImage = Insertable<ProjectImage>
+export type UpdateProjectImage = Updateable<ProjectImage>
+
+export type SelectProjectFeature = Selectable<ProjectFeature>
+export type InsertProjectFeature = Insertable<ProjectFeature>
+export type UpdateProjectFeature = Updateable<ProjectFeature>
+
+export type SelectTechnology = Selectable<Technology>
+export type InsertTechnology = Insertable<Technology>
+export type UpdateTechnology = Updateable<Technology>
