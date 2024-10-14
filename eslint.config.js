56bc414b18b5ec386bae8119461f08ab80585159
Manuel Can Kaya <manuelck99@hotmail.com>
2024-10-15 00:07:27 +0200

    Updated eslint

---
+++ eslint.config.js
@@ -0,0 +1,62 @@
+const { FlatCompat } = require("@eslint/eslintrc")
+const js = require("@eslint/js")
+const nxEslintPlugin = require("@nx/eslint-plugin")
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+})
+
+module.exports = [
+  { plugins: { "@nx": nxEslintPlugin } },
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    rules: {
+      "@nx/enforce-module-boundaries": [
+        "error",
+        {
+          enforceBuildableLibDependency: true,
+          allow: [],
+          depConstraints: [
+            {
+              sourceTag: "*",
+              onlyDependOnLibsWithTags: ["*"],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  ...compat.config({ extends: ["plugin:@nx/typescript"] }).map((config) => ({
+    ...config,
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      ...config.rules,
+    },
+  })),
+  ...compat.config({ extends: ["plugin:@nx/javascript"] }).map((config) => ({
+    ...config,
+    files: ["**/*.js", "**/*.jsx"],
+    rules: {
+      ...config.rules,
+    },
+  })),
+  ...compat.config({ env: { jest: true } }).map((config) => ({
+    ...config,
+    files: ["**/*.spec.ts", "**/*.spec.tsx", "**/*.spec.js", "**/*.spec.jsx"],
+    rules: {
+      ...config.rules,
+    },
+  })),
+  {
+    ignores: [
+      ".angular",
+      ".idea",
+      ".nx",
+      "dist",
+      "package-lock.json",
+      "**/*.css",
+      "**/*.scss",
+    ],
+  },
+]
